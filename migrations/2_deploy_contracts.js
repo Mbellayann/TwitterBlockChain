8ef4564fc56ee49c7132eb5b561627b3d70c00d5
gayathri <gayathrileka@gmail.com>
2021-05-31 12:28:53 +0200

    pulling

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-
-
-=======
 const Tweet = artifacts.require("./contracts/Tweet.sol");
 module.exports = function(deployer) {
   deployer.deploy(Tweet);
 };
->>>>>>> 5944cc88c721cbcf00791957cbe5b5084c38947e
 
